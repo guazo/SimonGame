58a5c1961a9c073b258d95135eb33bc980989538
Fernando O Guazo <fguazo@gnail.com>
2023-09-26 22:24:39 -0600

    user and random funcion souds and animation deployed

--- game.js
+++ game.js
@@ -1,11 +1,31 @@
+var userClickedPattern=[];
+var gamePattern=[];
+var userClickedPattern=[]
 var buttonColours =["red", "blue", "green", "yellow"];
+var randomChosenColour = buttonColours[nextSequence()];
 var gamePattern=[];
 
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
-    alert(randomNumber);
+return randomNumber;
+}
+function playSound(name){
+    var audioUser = new Audio('sounds/'+name+'.mp3');
+    audioUser.play();
+}
+function fadeFunction(name){
+    $('#'+name).fadeOut(300).fadeIn(300);
 }
 
 
+playSound(randomChosenColour);
+fadeFunction(randomChosenColour)
+gamePattern.push(randomChosenColour);
 
-alert(nextSequence());
+var userChosenColour=$("div").on("click", function(){
+playSound(this.id);
+fadeFunction(this.id);
+       return this.id;
+})
+userClickedPattern.push(userChosenColour);
+console.log("Color de la maquina: "+gamePattern); 
